0f178d26464ccbe363ace5db36c53cb44ba0850f
Jorge A <101210883+JAlbertbos@users.noreply.github.com>
2023-10-03 15:26:30 +0200

    pipe correcto

--- src/app/days/days.component.ts
+++ src/app/days/days.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { City, tripDays } from '../data/data';
-import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-days',
@@ -14,6 +13,7 @@ export class DaysComponent {
   filterCity: string = '';
   selectedFilter: string = 'city'; 
   filterValue: string = '';
+  errorMessage: string | null = null;
 
 
   // Función para mostrar los detalles del día seleccionado
@@ -21,11 +21,36 @@ export class DaysComponent {
     this.selectedDay = day;
   }
 
-  // Función para filtrar
+
+  // Función para filtrar la búsqueda
   applyFilter() {
- 
+    if (this.selectedFilter === 'city') {
+      // Buscar la ciudad relacionada con el texto ingresado
+      const filteredCity = tripDays.find(day => day.cityName.toLowerCase() === this.filterValue.toLowerCase());
+      
+      if (filteredCity) {
+        // Establecer la ciudad encontrada como día seleccionado
+        this.selectedDay = filteredCity;
+        this.errorMessage = null;
+      } else {
+        // Mostrar un mensaje de error si no se encuentra la ciudad
+        this.selectedDay = null;
+        this.errorMessage = "Ciudad no encontrada. Por favor, revisa el texto.";
+      }
+    } else {
+      // Reiniciar la lista de días y el mensaje de error
+      this.days = tripDays;
+      this.errorMessage = null;
+    }
   }
 
-
+   // Función para filtrar día seleccionado desde el desplegable
+   onDaySelect() {
+    const selectedDayNumber = parseInt(this.selectedFilter.replace('day', ''), 10);
+    const selectedDay = this.days.find(day => day.dayNumber === selectedDayNumber);
+    if (selectedDay) {
+      this.showDetail(selectedDay);
+    }
+  }
     
 }
