8bcc72978c6e93c92427037dcf73d1aec88a17f4
danielfreijo <146752570+danielfreijo@users.noreply.github.com>
2023-10-16 18:41:16 +0200

    Cambios de optimización

--- src/app/days/days.component.ts
+++ src/app/days/days.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { City, tripDays } from '../data/data';
 import { FormsModule } from '@angular/forms'; 
 
@@ -7,20 +7,28 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './days.component.html',
   styleUrls: ['./days.component.css']
 })
-export class DaysComponent {
-  dropdownOpen: boolean = false;
-  days: City[] = tripDays;
-  selectedDay: City | null = null;
-  filterCity: string = '';
-  selectedFilter: string = 'city'; 
-  filterValue: string = '';
 
+export class DaysComponent {
+  dropdownOpen: boolean = false; // Variable para rastrear si el menú desplegable del filtro está abierto
+  days: City[] = tripDays; // Arreglo que almacena información sobre los días de viaje
+  @Input() selectedDay: City | null = null; // Almacena el día seleccionado
+  @Output() daySelected = new EventEmitter<City>();
+  filterCity: string = ''; // Almacena el valor del filtro de ciudad
+  selectedFilter: string = 'city'; // Almacena el tipo de filtro seleccionado (inicialmente, por ciudad)
+  filterValue: string = ''; // Almacena el valor del filtro
+
+
+  // Método para alternar el estado del menú desplegable del filtro
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
   // Función para mostrar los detalles del día seleccionado
-  showDetail(day: City) {
-    this.selectedDay = day;
+  showDetails(day: City) {
+    if (this.selectedDay === day) {
+      this.selectedDay = null; // Deseleccionar el día si ya está seleccionado
+    } else {
+      this.selectedDay = day; // Seleccionar el día
+    }
   }
 
   // Función para filtrar
