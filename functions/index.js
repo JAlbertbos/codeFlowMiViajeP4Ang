5ade0c8bddb4b9870db8119b06334bea64ec3696
Jorge A <101210883+JAlbertbos@users.noreply.github.com>
2023-12-30 11:52:16 +0100

    changes

--- functions/index.js
+++ functions/index.js
@@ -5,54 +5,39 @@ const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 
 // The Firebase Admin SDK to access Firestore.
 const {initializeApp} = require("firebase-admin/app");
+const functions = require('firebase-functions');
 const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await getFirestore()
-        .collection("cities")
-        .add({original: original});
-    // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-  });
+exports.notifyOnCreateCity = functions.firestore
+    .document('/cities/{cityId}')
+    .onCreate((snapshot, context) => {
+        const cityId = context.params.cityId;
 
+        const payload = {
+            notification: {
+                title: 'Nueva ciudad agregada',
+                body: `Se ha creado una nueva ciudad con ID: ${cityId}`,
+            },
+        };
 
-  // All Realtime Database instances in default function region us-central1 at path "/user/{uid}"
-// There must be at least one Realtime Database present in us-central1.
-const onWrittenFunctionDefault = onValueWritten("/user/{uid}", (event) => {
-    // …
-  });
-  
-  // Instance named "my-app-db-2", at path "/user/{uid}".
-  // The "my-app-db-2" instance must exist in this region.
-  const OnWrittenFunctionInstance = onValueWritten(
-    {
-      ref: "/user/{uid}",
-      instance: "my-app-db-2"
-      // This example assumes us-central1, but to set location:
-      // region: "europe-west1"
-    },
-    (event) => {
-      // …
-    }
-  );
-  
-  // Instance with "my-app-db-" prefix, at path "/user/{uid}", where uid ends with @gmail.com.
-  // There must be at least one Realtime Database with "my-app-db-*" prefix in this region.
-  const onWrittenFunctionInstance = onValueWritten(
-    {
-      ref: "/user/{uid=*@gmail.com}",
-      instance: "my-app-db-*"
-      // This example assumes us-central1, but to set location:
-      // region: "europe-west1"
-    },
-    (event) => {
-      // …
-    }
-  );
+        return admin.messaging().sendToTopic('new_cities', payload);
+    });
+
+exports.notifyOnUpdateCity = functions.firestore
+    .document('/cities/{cityId}')
+    .onUpdate((change, context) => {
+        const newValue = change.after.data();
+        const previousValue = change.before.data();
+        const cityId = newValue.cityId;
+
+        const payload = {
+            notification: {
+                title: 'Ciudad actualizada',
+                body: `Se ha actualizado la ciudad con ID: ${cityId}`,
+            },
+        };
+
+        return admin.messaging().sendToTopic('updated_cities', payload);
+    });
